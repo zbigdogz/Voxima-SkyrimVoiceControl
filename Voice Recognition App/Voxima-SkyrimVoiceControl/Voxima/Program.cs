d665cae014af100c85bb4c7a83d13c7e3da87fb6
Exergist <exergist.me@gmail.com>
2023-04-13 22:29:32 -0500

    initial uploads #3
    Created Voxima application solution

---
+++ Voice Recognition App/Voxima-SkyrimVoiceControl/Voxima/Program.cs
@@ -0,0 +1,23 @@
+﻿// TODO:
+// Update the code marked with *** in comments
+// more testing for changing the port on the fly, as well as providing bad port information
+
+// zbigdogz DBU build path => ..\..\..\..\..\..\Games\Skyrim\MO2\Appdata\SkyrimSE\mods\Dragonborn Unlimited\SKSE\Plugins
+// exergist DBU build path => G:\Modding\Tools\Mod Organizer 2 - Games\Skyrim Special Edition\mods\Dragonborn Unlimited\SKSE\Plugins\DBU\Speech Recognition Application\
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voxima
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
